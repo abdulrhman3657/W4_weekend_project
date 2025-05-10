86c932053ec79d163a312291178e5e45310d9e81
user1 <user01@em/com>
2025-05-10 19:37:09 +0300

    update profile

--- scripts/addPost.js
+++ scripts/addPost.js
@@ -6,19 +6,17 @@ let post_img_url = document.getElementById("post_img_url");
 
 add_post_btn.addEventListener("click", () => {
 
-    // console.log(post_title.value)
-    // console.log(post_txt.value)
-    // console.log(post_img_url.value)
-
-
     let title = post_title.value;
     let text = post_txt.value;
     let url = post_img_url.value;
 
-
-    localStorage.setItem("title", title);
-    localStorage.setItem("text", text);
-    localStorage.setItem("url", url);
+    if(title != "" && text != "" && url != ""){
+        localStorage.setItem("title", title);
+        localStorage.setItem("text", text);
+        localStorage.setItem("url", url);
+    } else {
+        console.log("all values must be filled")
+    }
 
 
 })
--- scripts/profile.js
+++ scripts/profile.js
@@ -5,3 +5,20 @@ welcome_msg.innerText = `welcome back ${localStorage.getItem("username")}`;
 let data = document.getElementById("user-data");
 
 data.innerText += ` ${localStorage.getItem("username")}`;
+
+
+let user_post_title =  localStorage.getItem("title")
+
+if(user_post_title != null){
+    let user_post = document.getElementById("user-post");
+    user_post.classList.remove("d-none");
+
+    let card_img = document.getElementById("card-img");
+    card_img.src = localStorage.getItem("url");
+
+    let post_title = document.getElementById("post-title");
+    post_title.innerText = user_post_title;
+
+    let post_txt = document.getElementById("post-txt");
+    post_txt.innerText = localStorage.getItem("text");
+}
