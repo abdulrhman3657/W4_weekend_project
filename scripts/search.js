800e2253c460041d29361c89ec5416d14bc027bf
user1 <user01@em/com>
2025-05-11 07:00:08 +0300

    final v3

--- scripts/search.js
+++ scripts/search.js
@@ -19,6 +19,55 @@ let posts = [
         url: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/50/Venera_1_%28a%29_%28Memorial_Museum_of_Astronautics%29.JPG/500px-Venera_1_%28a%29_%28Memorial_Museum_of_Astronautics%29.JPG",
         tag: "spaceship",
         date: 1960
+    },
+    {
+        title: "Vostok 1",
+        post_text: "first human orbital spaceflight in history.",
+        url: "https://upload.wikimedia.org/wikipedia/en/b/b6/Vostok1_big.gif",
+        tag: "spaceship",
+        date: 1960
+    },
+    {
+        title: "Apollo 11",
+        post_text: "First crewed lunar landing",
+        url: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/Aldrin_Apollo_11_original.jpg/500px-Aldrin_Apollo_11_original.jpg",
+        tag: "spaceship",
+        date: 1960
+    },
+    {
+        title: "Luna 16",
+        post_text: "First robotic lunar sample returns",
+        url: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/bb/Luna_16.jpg/500px-Luna_16.jpg",
+        tag: "satellite",
+        date: 1970
+    },
+    {
+        title: "Salyut 1",
+        post_text: "First space station",
+        url: "https://upload.wikimedia.org/wikipedia/en/c/cc/Salyut_1.jpg",
+        tag: "spaceship",
+        date: 1970
+    },
+    {
+        title: "Mars 3",
+        post_text: "First Mars lander",
+        url: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9f/FP2A3620_%2823497688248%29_%28cropped%29.jpg/500px-FP2A3620_%2823497688248%29_%28cropped%29.jpg",
+        tag: "satellite",
+        date: 1970
+    },
+    {
+        title: "Venera 13",
+        post_text: "First recording of sound on another planet",
+        url: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8b/Venera_13_probe_-_illustrated.jpg/500px-Venera_13_probe_-_illustrated.jpg",
+        tag: "satellite",
+        date: 1980
+    },
+    {
+        title: "Mars Observer",
+        post_text: "Attempted Mars orbiter (contact lost)",
+        url: "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e5/Pathfinder01.jpg/500px-Pathfinder01.jpg",
+        tag: "satellite",
+        date: 1990
     }
 ]
 
@@ -28,6 +77,10 @@ type_btn.addEventListener("click", () => {
 
     let search = document.getElementById("search");
     search.classList.add("d-none");
+
+    let search2 = document.getElementById("search2");
+    search2.classList.add("d-none");
+
 
     let spaceship_input = document.getElementById("spaceship-input");
     let satellite_input = document.getElementById("satellite-input");
@@ -109,26 +162,327 @@ type_btn.addEventListener("click", () => {
             console.log(arr[i].title)
 
             let div = document.createElement("div")
+            div.classList.add("card");
+            div.classList.add("border-0");
+            div.classList.add("p-2");
+            div.classList.add("my-4");
+            div.classList.add("mx-4")
+            div.classList.add("rounded-3");
+            div.style.width = "18rem";
+            div.style.height = "35rem";
+
 
             let img = document.createElement("img")
             img.src = arr[i].url
+            img.classList.add("card-img-top");
+            img.style.height = "70%";
 
-            img.width = "300"
+            let div_body = document.createElement("div")
+            div_body.classList.add("card-body")
+            div_body.classList.add("d-flex")
+            div_body.classList.add("flex-column")
+            div_body.classList.add("justify-content-between")
+
+            let inner_div = document.createElement("div")
 
             let title = document.createElement("h3");
             title.innerText = arr[i].title;
+            title.classList.add("card-title")
 
             let text = document.createElement("p");
             text.innerText = arr[i].post_text;
-            
+            text.classList.add("card-text")
+
+
+            let card_btn = document.createElement("a");
+            card_btn.classList.add("btn")
+            card_btn.classList.add("btn-dark")
+            card_btn.innerText = "Read More"
+
+            inner_div.appendChild(title)
+            inner_div.appendChild(text)
+
+            div_body.appendChild(inner_div)
+            div_body.appendChild(card_btn)
+
             div.appendChild(img)
-            div.appendChild(title)
-            div.appendChild(text)
+            div.appendChild(div_body)
             place.appendChild(div)
         }
 
     } else {
         console.log("you must choose a tag")
+    }
+    
+
+})
+
+
+let date_btn = document.getElementById("date-btn");
+date_btn.addEventListener("click", () => {
+
+    let search = document.getElementById("search");
+    search.classList.add("d-none");
+    let search2 = document.getElementById("search2");
+    search2.classList.add("d-none");
+
+    let date_60 = document.getElementById("date-60");
+    let date_70 = document.getElementById("date-70");
+    let date_80 = document.getElementById("date-80");
+    let date_90 = document.getElementById("date-90");
+
+    if (date_60.checked){
+
+        let arr = posts.filter((post) => {
+            return post.date == 1960;
+        })
+
+        let place = document.getElementById("id1");
+
+        for(let i = 0; i < arr.length; i++){
+            console.log(arr[i].url)
+            console.log(arr[i].post_text)
+            console.log(arr[i].title)
+
+            let div = document.createElement("div")
+            div.classList.add("card");
+            div.classList.add("border-0");
+            div.classList.add("p-2");
+            div.classList.add("my-4");
+            div.classList.add("mx-4")
+            div.classList.add("rounded-3");
+            div.style.width = "18rem";
+            div.style.height = "35rem";
+
+
+            let img = document.createElement("img")
+            img.src = arr[i].url
+            img.classList.add("card-img-top");
+            img.style.height = "70%";
+
+            let div_body = document.createElement("div")
+            div_body.classList.add("card-body")
+            div_body.classList.add("d-flex")
+            div_body.classList.add("flex-column")
+            div_body.classList.add("justify-content-between")
+
+            let inner_div = document.createElement("div")
+
+            let title = document.createElement("h3");
+            title.innerText = arr[i].title;
+            title.classList.add("card-title")
+
+            let text = document.createElement("p");
+            text.innerText = arr[i].post_text;
+            text.classList.add("card-text")
+
+
+            let card_btn = document.createElement("a");
+            card_btn.classList.add("btn")
+            card_btn.classList.add("btn-dark")
+            card_btn.innerText = "Read More"
+
+            inner_div.appendChild(title)
+            inner_div.appendChild(text)
+
+            div_body.appendChild(inner_div)
+            div_body.appendChild(card_btn)
+
+            div.appendChild(img)
+            div.appendChild(div_body)
+            place.appendChild(div)
+        }
+
+
+    } else if (date_70.checked) {   
+
+                let arr = posts.filter((post) => {
+            return post.date == 1970;
+        })
+
+        let place = document.getElementById("id1");
+
+        for(let i = 0; i < arr.length; i++){
+            console.log(arr[i].url)
+            console.log(arr[i].post_text)
+            console.log(arr[i].title)
+
+            let div = document.createElement("div")
+            div.classList.add("card");
+            div.classList.add("border-0");
+            div.classList.add("p-2");
+            div.classList.add("my-4");
+            div.classList.add("mx-4")
+            div.classList.add("rounded-3");
+            div.style.width = "18rem";
+            div.style.height = "35rem";
+
+
+            let img = document.createElement("img")
+            img.src = arr[i].url
+            img.classList.add("card-img-top");
+            img.style.height = "70%";
+
+            let div_body = document.createElement("div")
+            div_body.classList.add("card-body")
+            div_body.classList.add("d-flex")
+            div_body.classList.add("flex-column")
+            div_body.classList.add("justify-content-between")
+
+            let inner_div = document.createElement("div")
+
+            let title = document.createElement("h3");
+            title.innerText = arr[i].title;
+            title.classList.add("card-title")
+
+            let text = document.createElement("p");
+            text.innerText = arr[i].post_text;
+            text.classList.add("card-text")
+
+
+            let card_btn = document.createElement("a");
+            card_btn.classList.add("btn")
+            card_btn.classList.add("btn-dark")
+            card_btn.innerText = "Read More"
+
+            inner_div.appendChild(title)
+            inner_div.appendChild(text)
+
+            div_body.appendChild(inner_div)
+            div_body.appendChild(card_btn)
+
+            div.appendChild(img)
+            div.appendChild(div_body)
+            place.appendChild(div)
+        }
+
+    } else if (date_80.checked) {
+
+                let arr = posts.filter((post) => {
+            return post.date == 1980;
+        })
+
+        let place = document.getElementById("id1");
+
+        for(let i = 0; i < arr.length; i++){
+            console.log(arr[i].url)
+            console.log(arr[i].post_text)
+            console.log(arr[i].title)
+
+            let div = document.createElement("div")
+            div.classList.add("card");
+            div.classList.add("border-0");
+            div.classList.add("p-2");
+            div.classList.add("my-4");
+            div.classList.add("mx-4")
+            div.classList.add("rounded-3");
+            div.style.width = "18rem";
+            div.style.height = "35rem";
+
+
+            let img = document.createElement("img")
+            img.src = arr[i].url
+            img.classList.add("card-img-top");
+            img.style.height = "70%";
+
+            let div_body = document.createElement("div")
+            div_body.classList.add("card-body")
+            div_body.classList.add("d-flex")
+            div_body.classList.add("flex-column")
+            div_body.classList.add("justify-content-between")
+
+            let inner_div = document.createElement("div")
+
+            let title = document.createElement("h3");
+            title.innerText = arr[i].title;
+            title.classList.add("card-title")
+
+            let text = document.createElement("p");
+            text.innerText = arr[i].post_text;
+            text.classList.add("card-text")
+
+
+            let card_btn = document.createElement("a");
+            card_btn.classList.add("btn")
+            card_btn.classList.add("btn-dark")
+            card_btn.innerText = "Read More"
+
+            inner_div.appendChild(title)
+            inner_div.appendChild(text)
+
+            div_body.appendChild(inner_div)
+            div_body.appendChild(card_btn)
+
+            div.appendChild(img)
+            div.appendChild(div_body)
+            place.appendChild(div)
+        }
+
+    } else if (date_90.checked) {
+
+
+                let arr = posts.filter((post) => {
+            return post.date == 1990;
+        })
+
+        let place = document.getElementById("id1");
+
+        for(let i = 0; i < arr.length; i++){
+            console.log(arr[i].url)
+            console.log(arr[i].post_text)
+            console.log(arr[i].title)
+
+            let div = document.createElement("div")
+            div.classList.add("card");
+            div.classList.add("border-0");
+            div.classList.add("p-2");
+            div.classList.add("my-4");
+            div.classList.add("mx-4")
+            div.classList.add("rounded-3");
+            div.style.width = "18rem";
+            div.style.height = "35rem";
+
+
+            let img = document.createElement("img")
+            img.src = arr[i].url
+            img.classList.add("card-img-top");
+            img.style.height = "70%";
+
+            let div_body = document.createElement("div")
+            div_body.classList.add("card-body")
+            div_body.classList.add("d-flex")
+            div_body.classList.add("flex-column")
+            div_body.classList.add("justify-content-between")
+
+            let inner_div = document.createElement("div")
+
+            let title = document.createElement("h3");
+            title.innerText = arr[i].title;
+            title.classList.add("card-title")
+
+            let text = document.createElement("p");
+            text.innerText = arr[i].post_text;
+            text.classList.add("card-text")
+
+
+            let card_btn = document.createElement("a");
+            card_btn.classList.add("btn")
+            card_btn.classList.add("btn-dark")
+            card_btn.innerText = "Read More"
+
+            inner_div.appendChild(title)
+            inner_div.appendChild(text)
+
+            div_body.appendChild(inner_div)
+            div_body.appendChild(card_btn)
+
+            div.appendChild(img)
+            div.appendChild(div_body)
+            place.appendChild(div)
+        }
+
+    } else {
+        console.log("you must choose a date")
     }
     
 
